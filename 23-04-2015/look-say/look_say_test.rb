4c8e1d2e1b3c595856ce52361e29789514151e8b
Subhash Shah <subhash.shah@botreeconsulting.com>
2015-04-23 15:34:00 +0530

    Updated tests to display expected vs got result

--- 23-04-2015/look-say/look_say_test.rb
+++ 23-04-2015/look-say/look_say_test.rb
@@ -4,32 +4,32 @@ require_relative 'look_say'
 class LookSayTest < Minitest::Test
 
   def test_next_1_for_1
-    assert LookSay.next(1).eql?(11)
+    assert_equal 11, LookSay.next(1)
   end
 
   def test_next_1_for_2
     skip
-    assert LookSay.next(1, 2).eql?([11, 21])
+    assert_equal [11, 21], LookSay.next(1, 2)
   end
 
   def test_next_1_for_3
     skip
-    assert LookSay.next(1, 3).eql?([11, 21, 1211])
+    assert_equal [11, 21, 1211], LookSay.next(1, 3)
   end
 
   def test_next_2_for_1211
     skip
-    assert LookSay.next(1211, 2).eql?([111221, 312211])
+    assert_equal [111221, 312211], LookSay.next(1211, 2)
   end
 
   def test_next_1_for_2466
     skip
-    assert LookSay.next(2466).eql?(121426)
+    assert_equal 121426, LookSay.next(2466)
   end
 
   def test_next_5_for_23
     skip
-    assert LookSay.next(23, 5).eql?([1213, 11121113, 31123113, 132112132113, 11131221121113122113])
+    assert_equal [1213, 11121113, 31123113, 132112132113, 11131221121113122113], LookSay.next(23, 5)
   end
 
 end
